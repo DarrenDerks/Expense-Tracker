9456a0237cc596ff7c4e05d6d4b6b314a0fc42a5
Darren Derks <leederksjr@gmail.com>
2021-03-21 07:04:07 -0600

    Did some styles on the main card and got the 3 cards in the order I wanted them on the page using @material-ui

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,12 +7,31 @@ import {
   Grid,
   Divider,
 } from "@material-ui/core";
-import classes from "*.module.css";
+
+import useStyles from "./styles";
 
 const Main = () => {
+  const classes = useStyles();
   return (
     <Card className={classes.root}>
       <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance $100
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: "1.5em", marginTop: "20px" }}
+        >
+          Try saying: Add income for $100 in Catgegory Salary for Monday...
+        </Typography>
+        <Divider />
+      </CardContent>
+      <CardContent className={classes.cardContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}></Grid>
+        </Grid>
+      </CardContent>
     </Card>
   );
 };
