317a553cac1b6b31ce485c9febc92ebbe4111fe2
Darren Derks <leederksjr@gmail.com>
2021-04-29 15:12:24 -0600

    Added functionality to side cards, records expense and income on doughnut chart

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Card, CardHeader, CardContent, Typography } from "@material-ui/core";
 import { Doughnut } from "react-chartjs-2";
+import useTransactions from "../../useTransactions";
 
-import useStyles from "../styles";
+import useStyles from "./styles";
 
-const Details = ({ title }) => {
+const DetailsCard = ({ title }) => {
   const classes = useStyles();
+  const { total, chartData } = useTransactions(title);
 
   return (
-    <Card className={title === 'Income' ? classes.income : classes.expense}>
+    <Card className={title === "Income" ? classes.income : classes.expense}>
       <CardHeader title={title} />
       <CardContent>
-        <Typography variant="h5">$50</Typography>
+        <Typography variant="h5">${total}</Typography>
+        <Doughnut data={chartData} />
       </CardContent>
     </Card>
   );
 };
 
-export default Details;
+export default DetailsCard;
