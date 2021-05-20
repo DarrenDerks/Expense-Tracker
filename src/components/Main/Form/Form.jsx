e05df70c5870468afd5a7f345c06058044a79749
Darren Derks <leederksjr@gmail.com>
2021-05-20 06:29:47 -0600

    Form is filled with speech, also the transaction is created with speech when form is filled. Added local storage for now.

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -34,6 +34,8 @@ const Form = () => {
   const { segment } = useSpeechContext();
 
   const createTransaction = () => {
+    if (Number.isNaN(Number(formData.amount)) || !formData.date.includes("-"))
+      return;
     const transaction = {
       ...formData,
       amount: Number(formData.amount),
@@ -47,6 +49,52 @@ const Form = () => {
     if (segment) {
       if (segment.intent.intent === "add_expense") {
         setFormData({ ...formData, type: "Expense" });
+      } else if (segment.intent.intent === "add_income") {
+        setFormData({ ...formData, type: "Income" });
+      } else if (
+        segment.isFinal &&
+        segment.intent.intent === "create_transaction"
+      ) {
+        return createTransaction();
+      } else if (
+        segment.isFinal &&
+        segment.intent.intent === "cancel_transactions"
+      ) {
+        return setFormData(initialState);
+      }
+
+      segment.entities.forEach((e) => {
+        const category = "${e.value.charAt(0)}${e.value.slice(1).toLowerCase()";
+
+        switch (e.type) {
+          case "amount":
+            setFormData({ ...formData, amount: e.value });
+            break;
+          case "category":
+            if (incomeCategories.map((iC) => iC.type).includes(category)) {
+              setFormData({ ...formData, type: "Income", category });
+            } else if (
+              expenseCategories.map((iC) => iC.type).includes(category)
+            ) {
+              setFormData({ ...formData, type: "Expense", category });
+            }
+            break;
+          case "date":
+            setFormData({ ...formData, date: e.value });
+            break;
+          default:
+            break;
+        }
+      });
+
+      if (
+        segment.isFinal &&
+        formData.amount &&
+        formData.category &&
+        formData.type &&
+        formData.date
+      ) {
+        createTransaction();
       }
     }
   }, [segment]);
