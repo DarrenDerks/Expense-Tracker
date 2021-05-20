bc9b984ea149d5e462970efaa38363255cd81dd6
Darren Derks <leederksjr@gmail.com>
2021-05-20 06:55:35 -0600

    Made total balance on middle card show the correct balance when adding transactions.

--- src/context/context.js
+++ src/context/context.js
@@ -2,7 +2,7 @@ import React, { useReducer, createContext } from "react";
 
 import contextReducer from "./contextReducer";
 
-const initialState = [];
+const initialState = JSON.parse(localStorage.getItem("transactions")) || [];
 
 export const ExpenseTrackerContext = createContext(initialState);
 
@@ -18,7 +18,11 @@ export const Provider = ({ children }) => {
     dispatch({ type: "ADD_TRANSACTION", payload: transaction });
   };
 
-  console.log(transactions);
+  const balance = transactions.reduce(
+    (acc, currVal) =>
+      currVal.type === "Expense" ? acc - currVal.amount : acc + currVal.amount,
+    0
+  );
 
   return (
     <ExpenseTrackerContext.Provider
@@ -26,6 +30,7 @@ export const Provider = ({ children }) => {
         deleteTransaction,
         addTransaction,
         transactions,
+        balance,
       }}
     >
       {children}
